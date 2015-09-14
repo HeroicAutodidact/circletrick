8692a590951d5d871b91691cced08ae04581f6d8
= <gwelkind@gmail.com>
2015-09-13 23:10:28 -0500

    Finished donut constructor and draw

--- js/app/donut.js
+++ js/app/donut.js
@@ -1,23 +1,63 @@
+"use strict";
+
 define(function(){
 	//Donut constructor
+
+	var dcan,dctx; //Canvas and context used exclusively for making donuts
+
 	var Donut = function(ctx,
 											 ir, //inner radius
 											 or, //outer radius
-											 fill,
+											 arcang, //arcangle
+											 fill,  //fill
 											 stroke,
-											 arc ){
+											 sw){ //strokewidth
 		this.ctx = ctx;
 		this.ir = ir;
 		this.or = or;
 		this.fill = fill;
 		this.stroke = stroke;
-		this.arc = arc;
+		this.arcang = arcang;
+		this.sw = sw;
+		this.center = this.or+this.sw*.5; //x and y coordinate of the donut's
+																				 //center (they will be the same).
+		if(!dcan){
+			this.createDcan();
+		};
+	};
+
+
+	Donut.prototype.createDcan = function(){
+		dcan = document.createElement('canvas');
+		dcan.width = this.ctx.canvas.width;
+		dcan.height = this.ctx.canvas.height;
+		dctx = dcan.getContext('2d');
 	};
 
 	//Draw a donut about the origin of current context
 	Donut.prototype.draw = function(){
-		// c.fillStyle = "skyblue";
-		// c.fillRect(0,0,canvas.width,canvas.height);
+
+		//Setup pallette
+		dctx.fillStyle = this.fill;
+		dctx.strokeStyle = this.stroke;
+		dctx.lineWidth = this.sw;
+
+		//Draw a filled arc
+		dctx.beginPath();
+		dctx.arc(this.center,this.center,this.or,0,this.arcang,true);
+		dctx.fill();
+		dctx.stroke();
+
+		//Subtract inner arc
+		dctx.beginPath();
+		dctx.arc(this.center,this.center,this.ir,0,this.arcang,true);
+		dctx.globalCompositeOperation = 'xor';
+		dctx.fill();
+
+		//Draw a
+		dctx.globalCompositeOperation = 'source-over';
+		dctx.stroke();
+		this.ctx.drawImage(dcan,0,0);
 
 		// //test code to make a masking operation
 		// var maskCanvas = document.createElement('canvas');
--- js/app/main.js
+++ js/app/main.js
@@ -2,21 +2,10 @@ define(function(require){
 		var Donut = require('./donut');
 		var canvas = document.getElementById('theCanvas');
 		var c = canvas.getContext('2d');
-		d = new Donut(c, 10, 20, 'black', 'gray');
-		alert(d);
-		// c.fillStyle = "skyblue";
-		// c.fillRect(0,0,canvas.width,canvas.height);
 
-		// //test code to make a masking operation
-		// var maskCanvas = document.createElement('canvas');
-		// maskCanvas.width = canvas.width;
-		// maskCanvas.height = canvas.height;
-		// var maskCtx = maskCanvas.getContext('2d');
-
-		// maskCtx.fillStyle = "black";
-		// maskCtx.fillRect(0,0, maskCanvas.width, maskCanvas.height);
-		// maskCtx.globalCompositeOperation = 'xor';
-		// maskCtx.arc(30,30,10,0,2*Math.PI);
-		// maskCtx.fill();
-		// c.drawImage(maskCanvas,0,0);
+		//!//
+		c.translate(canvas.width/2,canvas.height/2);
+		d = new Donut(c, 20, 50, 2*Math.PI,'grey', 'black', 5);
+		d.draw();
+		//~!//
 });
